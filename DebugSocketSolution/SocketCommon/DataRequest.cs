3de7b18a124c085467d4992899d9735e69431129
Alexander Shaduro <belpyro@gmail.com>
2014-10-07 19:42:49 +0300

    + added initial sources

---
+++ DebugSocketSolution/SocketCommon/DataRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SocketCommon
+{
+    [Serializable]
+    public class DataRequest
+    {
+        public DataRequest(string name, Commands command)
+        {
+            TypeName = name;
+            Command = command;
+        }
+
+        public string TypeName { get; private set; }
+
+        public Commands Command { get; private set; }
+    }
+}
---
+++ SocketServer/Server.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PluginFramework;
+
+namespace SocketServer
+{
+    [KSPAddon(KSPAddon.Startup.EveryScene, false)]
+    public class Server: MonoBehaviourExtended
+    {
+
+    }
+}
