fdd399f11cc33b66bd9a768edbb58c70680b23e3
Alexander Shaduro <belpyro@gmail.com>
2014-12-27 23:15:45 +0300

    cecil

--- DebugSocketSolution/CecilTest/Program.cs
+++ DebugSocketSolution/CecilTest/Program.cs
@@ -15,17 +15,17 @@ namespace CecilTest
     {
         static void Main(string[] args)
         {
-            var assembly = AssemblyDefinition.ReadAssembly(@"e:\KSP_win\KSP_Data\Managed\Assembly-CSharp.dll");
+            var assembly = AssemblyDefinition.ReadAssembly(@"e:\KSP_Dev_25\KSP_Data\Managed\Assembly-CSharp.dll.old");
 
-            //var logMethod = typeof(Debug).GetMethod("Log", new[] { typeof(object) });
+            var logMethod = typeof(Debug).GetMethod("Log", new[] { typeof(object) });
 
-            //var refrToLog = assembly.MainModule.Import(logMethod);
+            var refrToLog = assembly.MainModule.Import(logMethod);
 
             //var attrCtor = assembly.MainModule.Import(typeof (SerializableAttribute).GetConstructor(Type.EmptyTypes));
 
             var kspMenuType =
                 assembly.MainModule.Types.FirstOrDefault(
-                    x => x.Name.Equals("TutorialWindow", StringComparison.InvariantCultureIgnoreCase));
+                    x => x.Name.Equals("EditorLogic", StringComparison.InvariantCultureIgnoreCase));
 
             //foreach (var typeDefinition in assembly.MainModule.Types)
             //{
@@ -33,21 +33,22 @@ namespace CecilTest
             //}
             if (kspMenuType != null)
             {
-                var definition = kspMenuType.Methods.FirstOrDefault(x => x.Name.Equals("Start"));
+                //var definition = kspMenuType.Methods.FirstOrDefault(x => x.Name.Equals("Start"));
 
-                if (definition != null)
-                {
-                    definition.IsPublic = true;
-                    definition.IsVirtual = true;
-                }
-                //foreach (var methodDefinition in kspMenuType.Methods.Where(x => !x.IsConstructor))
+                //if (definition != null)
                 //{
-                //    methodDefinition.Body.Instructions.Insert(0, Instruction.Create(OpCodes.Ldstr, "Injected"));
-                //    methodDefinition.Body.Instructions.Insert(1, Instruction.Create(OpCodes.Call, refrToLog));
+                //    definition.IsPublic = true;
+                //    definition.IsVirtual = true;
                 //}
+                foreach (var methodDefinition in kspMenuType.Methods.Where(x => !x.IsConstructor && !x.Name.Equals("Update") && !x.Name.Equals("OnGUI")
+                    && !x.Name.Equals("UpdatePartMode") && !x.Name.Equals("mouseOverModalArea") && !x.Name.Equals("get_SelectedPart")))
+                {
+                    methodDefinition.Body.Instructions.Insert(0, Instruction.Create(OpCodes.Ldstr, string.Format("Called {0}",methodDefinition.Name)));
+                    methodDefinition.Body.Instructions.Insert(1, Instruction.Create(OpCodes.Call, refrToLog));
+                }
             }
 
-            assembly.Write(@"e:\Assembly-CSharp.dll");
+            assembly.Write(@"e:\KSP_Dev_25\KSP_Data\Managed\Assembly-CSharp.dll");
 
         }
     }
